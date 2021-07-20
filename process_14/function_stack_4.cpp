9ad871e8dea8cccea3b82a1c265d3a9867a41dfe
rere <xiangli_scut@qq.com>
2021-07-20 10:45:05 +0800

    1. process_14 : 理解stack frame（函数调用栈）、rbp-rsp的切换、参数利用寄存器传递并复制至栈帧中。附对应asm代码（网上g++转换）

---
+++ process_14/function_stack_4.cpp
@@ -0,0 +1,33 @@
+//
+// Created by rere on 2021/7/19.
+//
+
+#include <cstdio>
+
+int func_2() {
+    return -1;
+}
+
+int func_1(int a, int b, int c, int d) {
+    int e, f;
+    long long g;
+
+    e = 1;
+    f = 2;
+    g = 3;
+
+    func_2();
+
+    e = a + b + c + d;
+
+    return e ;
+}
+
+int main(int argc, char* argv[]) {
+    int f = 0;
+    int g = 1;
+    f = func_1(1, 2, 3, 4+g);
+
+    printf("value: %d\n", f);
+    return 0;
+}
---
+++ process_14/function_stack_5.cpp.s
@@ -0,0 +1,57 @@
+func_2():
+        push    rbp
+        mov     rbp, rsp
+        mov     eax, -1
+        pop     rbp
+        ret
+func_1(int, int, int, int, int):
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 40
+        mov     DWORD PTR [rbp-20], edi
+        mov     DWORD PTR [rbp-24], esi
+        mov     DWORD PTR [rbp-28], edx
+        mov     DWORD PTR [rbp-32], ecx
+        mov     DWORD PTR [rbp-36], r8d
+        mov     DWORD PTR [rbp-4], 1
+        mov     DWORD PTR [rbp-8], 2
+        mov     QWORD PTR [rbp-16], 3
+        call    func_2()
+        mov     edx, DWORD PTR [rbp-20]
+        mov     eax, DWORD PTR [rbp-24]
+        add     edx, eax
+        mov     eax, DWORD PTR [rbp-28]
+        add     edx, eax
+        mov     eax, DWORD PTR [rbp-32]
+        add     edx, eax
+        mov     eax, DWORD PTR [rbp-36]
+        add     eax, edx
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-4]
+        leave
+        ret
+.LC0:
+        .string "value: %d\n"
+main:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     DWORD PTR [rbp-4], 0
+        mov     DWORD PTR [rbp-8], 1
+        mov     eax, DWORD PTR [rbp-8]
+        add     eax, 4
+        mov     r8d, 5
+        mov     ecx, eax
+        mov     edx, 3
+        mov     esi, 2
+        mov     edi, 1
+        call    func_1(int, int, int, int, int)
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-4]
+        mov     esi, eax
+        mov     edi, OFFSET FLAT:.LC0
+        mov     eax, 0
+        call    printf
+        mov     eax, 0
+        leave
+        ret
